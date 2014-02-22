4e0787468cf2e0328a729012339d2c0ad93e09b5
Max Prokopiev <me@maxprokopiev.com>
2014-02-22 16:46:21 +0400

    Remove keyboard modifiers from osx/defaults

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -7,12 +7,6 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 # Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# Disable press-and-hold for keys in favor of key repeat
-defaults write -g ApplePressAndHoldEnabled -bool false
-
-# Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
-
 # Save screenshots to the desktop
 defaults write com.apple.screencapture location -string "$HOME/Downloads"
 
