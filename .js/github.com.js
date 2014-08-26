9eb32e6e3389728d62ce5b1fb6b42c0a071b4d9c
Max Prokopiev <me@maxprokopiev.com>
2014-08-26 15:56:46 +0400

    Fix github.com.js script for Expand/Collapse buttons

--- .js/github.com.js
+++ .js/github.com.js
@@ -1,16 +1,27 @@
-$(document).ready(function() {
-  $('#files.diff-view .js-details-container .button-group').append('<li><a class="grouped-button minibutton bigger lighter collapse" href="#">Collapse<code></code></a></li>');
-  $('#files.diff-view .js-details-container .button-group').append('<li><a class="grouped-button minibutton bigger lighter collapse" href="#" style="display:none;">Expand<code></code></a></li>');
-  $('#toc p.explain').append('<a href="#" class="minibutton global-collapse" style="margin-right: 10px;">Collapse All</a>')
-  $('#toc p.explain').append('<a href="#" class="minibutton global-collapse" style="display:none; margin-right: 10px;">Expand All</a>')
+function setup_buttons_per_diff() {
+  var collapse_btn = '<a href="#" class="minibutton tooltipped tooltipped-n collapse" aria-label="Collapse diff">Collapse</a>',
+      expand_btn   = '<a href="#" class="minibutton tooltipped tooltipped-n collapse" aria-label="Expand hidden diff" style="display:none">Expand</a>'
 
-  $('a.global-collapse').on('click', function() {
-    $('.js-details-container .image, .data:not(".suppressed"), .collapse, .global-collapse').toggle();
-    return false;
-  });
+  $('#files.diff-view .js-details-container .actions').prepend(collapse_btn);
+  $('#files.diff-view .js-details-container .actions').prepend(expand_btn);
 
   $('a.collapse').on('click', function() {
     $(this).parents('.js-details-container').find('.image, .data:not(".suppressed"), .collapse').toggle();
     return false;
   });
+}
+
+function setup_global_buttons() {
+  $('#toc').prepend('<a href="#" class="minibutton right global-collapse" style="margin-left: 10px;">Collapse All</a>')
+  $('#toc').prepend('<a href="#" class="minibutton right global-collapse" style="display:none; margin-left: 10px;">Expand All</a>')
+
+  $('a.global-collapse').on('click', function() {
+    $('.js-details-container .image, .data:not(".suppressed"), .collapse, .global-collapse').toggle();
+    return false;
+  });
+}
+
+$(document).ready(function() {
+  setup_buttons_per_diff();
+  setup_global_buttons();
 });
